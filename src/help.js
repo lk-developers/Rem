5e6f3abcd3400c043f5c68306ada627f89474df2
ipmanlk <io@navinda.xyz>
2020-10-19 17:21:45 +0530

    change: move help embed to a seperate js

---
+++ src/help.js
@@ -0,0 +1,100 @@
+const getHelpEmbed = (prefix) => {
+	return {
+		description: "Send ♥ to themes.moe and jikan.moe for their amazing work!.",
+		author: {
+			name: "| Help",
+			icon_url: "https://tinyurl.com/y4x8xlat",
+			url: "https://github.com/lk-developers",
+		},
+		fields: [
+			{
+				name: "Main commands",
+				value: "----------------------------------------",
+			},
+			{
+				name: `${prefix}play <song name> or <youtube url> or <youtube playlist url>`,
+				value: "Start playing a track from youtube.",
+			},
+			{
+				name: `${prefix}anime <anime name>`,
+				value: "Start playing OPs & EDs from themes.moe.\n\u200B",
+			},
+			{
+				name: "Basic controls",
+				value: "----------------------------------------",
+			},
+			{
+				name: `${prefix}pause`,
+				value: "Pause player.",
+				inline: true,
+			},
+			{
+				name: `${prefix}play`,
+				value: "Resume player.",
+				inline: true,
+			},
+			{
+				name: `${prefix}stop`,
+				value: "Stop player and clear the queue.",
+				inline: true,
+			},
+			{
+				name: "Queue commands",
+				value: "----------------------------------------",
+			},
+			{
+				name: `${prefix}queue`,
+				value: "Show queue.",
+				inline: true,
+			},
+			{
+				name: `${prefix}skip`,
+				value: "Skip to next track.",
+				inline: true,
+			},
+			{
+				name: `${prefix}skip <number>`,
+				value: "Jump to a track in the queue.",
+				inline: true,
+			},
+			{
+				name: "Playlist commands",
+				value: "----------------------------------------",
+			},
+			{
+				name: `${prefix}plplay`,
+				value: "Start your playlist (add to queue).",
+				inline: true,
+			},
+			{
+				name: `${prefix}pladd`,
+				value: "Add current track to your playlist.",
+				inline: true,
+			},
+			{
+				name: `${prefix}plshow`,
+				value: "Show your playlist.",
+				inline: true,
+			},
+			{
+				name: `${prefix}pldel <number>`,
+				value: "Remove track from your playlist.\n\u200B",
+				inline: true,
+			},
+			{
+				name: "Other commands",
+				value: "----------------------------------------",
+			},
+			{
+				name: `${prefix}help`,
+				value: "Show help.",
+			},
+		],
+		footer: {
+			text: "By LK Developers",
+			icon_url: "https://tinyurl.com/yyhwlrqc",
+		},
+	};
+};
+
+module.exports = { getHelpEmbed };
