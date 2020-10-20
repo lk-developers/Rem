14c09d42fc752d3bf6c2ebaeb3d8350c838abe48
ipmanlk <io@navinda.xyz>
2020-10-20 16:47:21 +0530

    add: funhelp command

---
+++ src/commands/fun/misc/funhelp.js
@@ -0,0 +1,36 @@
+const config = require(`${process.cwd()}/config/config.json`);
+
+const handle = (message) => {
+	const embed = getHelpEmbed(config.PREFIX);
+	message.reply({ embed: embed });
+};
+
+const getHelpEmbed = (prefix) => {
+	return {
+		color: "#7ca8d9",
+		description:
+			"_What’s wrong with taking the path that’s easiest, that will let me live longer?._",
+		author: {
+			name: "| Help",
+			icon_url: "https://tinyurl.com/y4x8xlat",
+			url: "https://github.com/lk-developers",
+		},
+		thumbnail: {
+			url: "https://i.imgur.com/B6N9S7l.gif",
+		},
+		fields: [
+			{
+				name: `${prefix}remme`,
+				value: "Show your dp with rem.",
+			},
+			{
+				name: `${prefix}remgif`,
+				value: "Show a random rem gif.",
+			},
+		],
+	};
+};
+
+module.exports = {
+	handle,
+};
